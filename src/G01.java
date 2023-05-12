541d2ba2e43ad1ead884872c8045ea4435ec7e59
zaferakarsu <zaakarsu53@gmail.com>
2023-05-12 16:15:50 +0300

    4.gün

---
+++ src/G01.java
@@ -0,0 +1,5 @@
+public class G01 {
+    public static void main(String[] args) {
+        System.out.println("Git/Github Tekrar ve Branch Kullanımı");
+    }
+}
---
+++ src/G02.java
@@ -0,0 +1,21 @@
+public class G02 {
+
+    /*
+    Git-Github'da projelerimizin üzerinde çalışırken temelde 3 adımda işlem yapıyoruz
+
+    1 - Projelerimizi/Dosyalarımızı git ile ilişkilendiriyoruz
+              git init
+
+    2 - projemizde/dosyamızda bulunanları versiyon oluşturuyorduk
+              git add .
+              git commit -m "versiyonadi"
+
+    3 - github'a yollama/entegrasyonu
+              git remote add origin repoUrlAdresi
+              already origin
+              git remote set-url repoUrlAdresi
+
+              git push
+
+    */
+}
